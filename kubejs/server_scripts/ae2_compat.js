eab7fd6af50e0b278cea590ca251d94b1db7823a
Sinewhales <whalessin12@gmail.com>
2025-05-28 22:24:40 +0200

    Tout les combs sont fait
    + Terralith
    + Shop
    + EMI
    -Frycooks

---
+++ kubejs/server_scripts/ae2_compat.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes(event => {
+event.custom({
+   "type": "tfmg:polarizing",
+  "ingredients": [
+    {
+      "item": "ae2:certus_quartz_crystal"
+    }
+  ],
+  "processingTime": 250,
+  "results": [
+    {
+      "item": 'ae2:charged_certus_quartz_crystal'
+    }
+  ],
+  "energy": 160
+})
+
+})
---
+++ kubejs/server_scripts/duplicates.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event => {
+event.remove({ id: 'createnuclear:mixing/steel' })
+event.remove({ id: 'createloveandwar:mixing/steel_ingot' })
+
+})
