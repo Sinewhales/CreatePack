4e22394fb11c66ffaf239054b111d7673107f207
Sinewhales <whalessin12@gmail.com>
2025-05-28 23:48:14 +0200

    vasy fini pour ajd

--- kubejs/server_scripts/ae2_compat.js
+++ kubejs/server_scripts/ae2_compat.js
@@ -14,5 +14,19 @@ event.custom({
   ],
   "energy": 160
 })
-
+event.custom({
+    "type": "create_new_age:energising",
+  "energy_needed": 160,
+  "ingredients": [
+    {
+      "item": "ae2:certus_quartz_crystal"
+    }
+  ],
+  "results": [
+    {
+      "item": 'ae2:charged_certus_quartz_crystal'
+    }
+  ]
+})
+event.remove({ id: 'ae2:charger/charged_certus_quartz_crystal' })
 })
--- kubejs/server_scripts/duplicates.js
+++ kubejs/server_scripts/duplicates.js
@@ -1,5 +1,6 @@
 ServerEvents.recipes(event => {
-event.remove({ id: 'createnuclear:mixing/steel' })
 event.remove({ id: 'createloveandwar:mixing/steel_ingot' })
+// Steel car les crafts de TFMG sont bugués donc c'est plus simple comme ça
+event.recipes.create.mixing('tfmg:steel_ingot', ['minecraft:coal', 'tfmg:blasting_mixture']).superheated()
 
 })
