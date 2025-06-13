1d24bf830b3b76f241aca874711735062f17ee7c
Sinewhales <whalessin12@gmail.com>
2025-06-13 15:32:05 +0200

    + trucs - trucs
    + Integrated Dynamics et Addons
    + Forestry
    + Gendustry
    + Mo'Bees
    + Mini Utilities
    + Construction Wand
    + Exchangers
    
    -Complicated Bees
    -Iron Furnaces
    -TFMG (trop bugué)

--- kubejs/server_scripts/ae2_compat.js
+++ kubejs/server_scripts/ae2_compat.js
@@ -1,20 +1,5 @@
 ServerEvents.recipes(event => {
 event.custom({
-   "type": "tfmg:polarizing",
-  "ingredients": [
-    {
-      "item": "ae2:certus_quartz_crystal"
-    }
-  ],
-  "processingTime": 250,
-  "results": [
-    {
-      "item": 'ae2:charged_certus_quartz_crystal'
-    }
-  ],
-  "energy": 160
-})
-event.custom({
     "type": "create_new_age:energising",
   "energy_needed": 160,
   "ingredients": [
--- kubejs/server_scripts/duplicates.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-event.remove({ id: 'createloveandwar:mixing/steel_ingot' })
-// Steel car les crafts de TFMG sont bugués donc c'est plus simple comme ça
-event.recipes.create.mixing('tfmg:steel_ingot', ['minecraft:coal', 'tfmg:blasting_mixture']).superheated()
-
-})
