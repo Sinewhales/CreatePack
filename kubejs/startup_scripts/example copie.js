94f1153b7fdd96304041841c9aff99526de2b5dd
Sinewhales <whalessin12@gmail.com>
2025-06-30 13:24:27 +0200

    def
    final

---
+++ kubejs/startup_scripts/example copie.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('fluid', event => {
+  // Basic "thick" (looks like lava) fluid with red tint
+  event.create('ambrosia')
+    .thinTexture(0xFFC0CB)
+    .bucketColor(0xFFC0CB)
+    .displayName('Ambrosia')
+                 
+
+})
--- kubejs/startup_scripts/example.js
+++ kubejs/startup_scripts/example.js
@@ -2,4 +2,9 @@ StartupEvents.registry('item', event => {
   // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
   // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
   event.create('iron_comb').displayName('Iron Comb')
+  event.create('advanced_mechanism').displayName('Advanced Mechanism')
+  event.create('unfinished_receptacle', 'create:sequenced_assembly').displayName('Incomplete Receptacle')
+  event.create('unfinished_genetics', 'create:sequenced_assembly').displayName('Incomplete Genetics Processor')
+  event.create('unfinished_power', 'create:sequenced_assembly').displayName('Incomplete Power Module')
+  event.create('unfinished_environmental', 'create:sequenced_assembly').displayName('Incomplete Environmental Processor')
 })
